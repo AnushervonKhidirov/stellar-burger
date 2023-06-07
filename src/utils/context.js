187e5109377e00528a8765128894d5077aeb08b8
AnushervonKhidirov <ankhid98@gmail.com>
2023-06-07 21:10:40 +0500

    modal context replaced by redux

--- src/utils/context.js
+++ src/utils/context.js
@@ -3,4 +3,3 @@ import { createContext } from 'react'
 export const BurgerConstructorContext = createContext()
 export const ConstructorContext = createContext()
 export const IngredientContext = createContext()
-export const ModalContext = createContext()
