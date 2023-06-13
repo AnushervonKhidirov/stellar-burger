761d388a69296bb225cbb1b532d932f20ce5af7d
AnushervonKhidirov <ankhid98@gmail.com>
2023-06-13 16:40:51 +0500

    get ingredients request replaced to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { loadIngredient } from './store/ingredientListSlice'
+
 import Header from './components/common/header/Header'
-import Constructor from './components/pages/constructor/Constructor'
+import Constructor from './pages/constructor/Constructor'
 import Modal from './components/common/modal/Modal'
 
 function App() {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(loadIngredient())
+    }, [dispatch])
+
     return (
         <>
             <Header />
