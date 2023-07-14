8034cf73066fef012a3ea71915e8cc73665b0b51
AnushervonKhidirov <ankhid98@gmail.com>
2023-07-14 13:52:16 +0500

    chage conditions for OrderBlock component

--- src/components/constructor-page-components/order-block/OrderBlock.tsx
+++ src/components/constructor-page-components/order-block/OrderBlock.tsx
@@ -20,22 +20,17 @@ const OrderBlock: FC = () => {
         const isSauce = constructorList.findIndex(ingredient => ingredient.type === 'sauce') !== -1
         const isAvailableToOrder = isBun && isMain
 
-        if (isAvailableToOrder) {
-            dispatch(
-                sendIngredientsId([
-                    ...constructorList.map(ingredient => ingredient._id),
-                    constructorBun._id,
-                ])
-            )
-        } else if (!isBun && isMain) {
-            alert("You can't eat burger without buns. Peak a bun)")
-        } else if (!isMain && isBun) {
-            alert("You can't eat only bun, it isn't tasty(")
-        } else if (isSauce && !isAvailableToOrder) {
-            alert("We think you don't want to order only sauce)")
-        } else {
-            alert('Your attempt to order nothing is failed!')
-        }
+        if (!isAvailableToOrder) return alert('Your attempt to order nothing is failed!')
+        if (!isBun && isMain) return alert("You can't eat burger without buns. Peak a bun)")
+        if (!isMain && isBun) return alert("You can't eat only bun, it isn't tasty(")
+        if (isSauce && !isAvailableToOrder) return alert("We think you don't want to order only sauce)")
+
+        dispatch(
+            sendIngredientsId([
+                ...constructorList.map(ingredient => ingredient._id),
+                constructorBun._id,
+            ])
+        )
     }
 
     return (
