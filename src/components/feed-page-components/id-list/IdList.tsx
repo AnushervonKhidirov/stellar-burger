eabe008aa03b2720cc8f7a1be86592552ab1165e
AnushervonKhidirov <ankhid98@gmail.com>
2023-08-02 17:50:16 +0500

    remove scrolling

--- src/components/feed-page-components/id-list/IdList.tsx
+++ src/components/feed-page-components/id-list/IdList.tsx
@@ -1,6 +1,5 @@
 import type { FC } from 'react'
-
-import { v4 as uuidV4 } from 'uuid'
+import { useEffect, useState } from 'react'
 
 import styles from './IdList.module.css'
 
@@ -11,11 +10,25 @@ interface IIdList {
 }
 
 const IdList: FC<IIdList> = ({ title, list, ready }) => {
+    const [splicedList, setSplicedList] = useState<(string | number)[]>([])
+
+    useEffect(() => {
+        if (list.length > 0) {
+            if (list.length <= 15) {
+                setSplicedList(list)
+            } else {
+                const newList = list.slice(0, 14)
+                newList.push('...')
+                setSplicedList(newList)
+            }
+        }
+    }, [list])
+
     return (
         <div className={styles.id_list_wrapper}>
             <h3 className='text text_type_main-medium mb-6'>{title}:</h3>
-            <ul className={`${styles.id_list} ${ready ? styles.id_list_ready : ''} text text_type_digits-default custom-scroll`}>
-                {list.map(orderId => <li key={uuidV4()}>{orderId}</li>)}
+            <ul className={`${styles.id_list} ${ready ? styles.id_list_ready : ''} text text_type_digits-default`}>
+                {splicedList.map((orderId) => <li key={orderId}>{orderId}</li>)}
             </ul>
         </div>
     )
