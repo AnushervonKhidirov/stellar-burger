ab7cbea0ada0e23ca84d12528b8967400bfeb005
AnushervonKhidirov <ankhid98@gmail.com>
2023-08-04 10:04:35 +0500

    update token added

--- src/middlewares/wsMiddleware.ts
+++ src/middlewares/wsMiddleware.ts
@@ -1,5 +1,7 @@
 import type { Middleware } from 'redux'
 import { ActionCreatorWithPayload, ActionCreatorWithoutPayload } from '@reduxjs/toolkit'
+import { updateToken } from '../utils/burger-api'
+import { JWT_EXPIRED_MESSAGE_WS } from '../utils/constants'
 
 export type TWsActionTypes = {
     readonly wsConnect: ActionCreatorWithPayload<string>
@@ -35,7 +37,8 @@ export const wsMiddleware: TWsMiddleware = wsActions => store => next => action 
             const { data } = event
             const parsedData = JSON.parse(data)
 
-            dispatch(wsMessage(parsedData))
+            if (parsedData.success) dispatch(wsMessage(parsedData))
+            if (parsedData.message === JWT_EXPIRED_MESSAGE_WS) updateToken()
         }
 
         socket.onclose = () => {
