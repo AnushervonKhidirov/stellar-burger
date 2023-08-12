96d5c22e3cc905e67a2fdc8600825a7bb0a9897d
AnushervonKhidirov <ankhid98@gmail.com>
2023-08-12 14:39:54 +0500

    cypress added and tested DnD funcionality

---
+++ cypress/e2e/drag_and_drop.cy.ts
@@ -0,0 +1,31 @@
+describe('My First Test', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080)
+
+        cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' })
+        cy.intercept('POST', 'api/orders', { fixture: 'order.json' }).as('postOrder')
+
+        window.localStorage.setItem('refreshToken', JSON.stringify('test-refreshToken'))
+        window.localStorage.setItem('accessToken', JSON.stringify('test-accessToken'))
+    })
+
+    it('visit my site', () => {
+        cy.visit('localhost:3000')
+
+		cy.get(`[data-testid='ingredient_item-643d69a5c3f7b9001cfa093c']`).trigger('dragstart')
+		cy.get(`[data-testid='constructor_block']`).trigger('drop')
+			.get(`[data-testid='ingredient_item-643d69a5c3f7b9001cfa093c']`).should('be.visible')
+		
+
+		cy.get(`[data-testid='ingredient_item-643d69a5c3f7b9001cfa0940']`).trigger('dragstart')
+		cy.get(`[data-testid='constructor_block']`).trigger('drop')
+			.get(`[data-testid='ingredient_item-643d69a5c3f7b9001cfa0940']`).should('be.visible')
+
+		cy.get(`[data-testid='ingredient_item-643d69a5c3f7b9001cfa093e']`).trigger('dragstart')
+		cy.get(`[data-testid='constructor_block']`).trigger('drop')
+			.get(`[data-testid='ingredient_item-643d69a5c3f7b9001cfa093e']`).should('be.visible')
+
+		cy.get(`[data-testid='constructor_ingredient-643d69a5c3f7b9001cfa093e'] .constructor-element__action`).trigger('click')
+			.get(`[data-testid='constructor_ingredient-643d69a5c3f7b9001cfa093e']`).should('not.exist')
+	})
+})
